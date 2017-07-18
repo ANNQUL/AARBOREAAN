2794082645e77670db3b82134cd5abcec26bd417
ferpalaciosd <ferpalaciosd@gmail.com>
2017-07-18 22:11:06 +0200

    Changed the font from Alegreya to EB Garamond.

--- AARBOREAAN/header.php
+++ AARBOREAAN/header.php
@@ -5,13 +5,13 @@
         <title>
             <?php echo page_title("Page can't be found"); ?> - <?php echo site_name(); ?>
         </title>
-        
+
         <!-- Asset links -->
-        <link href='https://fonts.googleapis.com/css?family=Alegreya:400,400italic|Playfair+Display:400,700' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
         <link href="<?php echo theme_url('assets/style.css'); ?>" media="screen" rel="stylesheet" type="text/css" />
         <link rel="shortcut icon" href="<?php echo theme_url('assets/favicon.ico'); ?>" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="/plugins/share-anchor/css/share-anchor.css"> <script type="text/javascript" src="/plugins/share-anchor/js/jquery.js"></script> <script type="text/javascript" src="/plugins/share-anchor/js/share-anchor.js"></script>
-        
+
         <!-- Meta -->
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="generator" content="Anchor CMS">
