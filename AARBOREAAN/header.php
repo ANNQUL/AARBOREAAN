dbb271a82c2f0529b5e0859909680088077b7ce9
ferpalaciosd <ferpalaciosd@gmail.com>
2017-08-31 17:27:25 +0200

    Added Animations

--- AARBOREAAN/header.php
+++ AARBOREAAN/header.php
@@ -16,6 +16,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="generator" content="Anchor CMS">
         <meta name=viewport content="width=device-width, initial-scale=1">
+
     </head>
     <body>
         <section class="group">
